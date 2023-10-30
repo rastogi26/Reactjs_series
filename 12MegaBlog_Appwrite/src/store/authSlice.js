1432a85079c40cfe387b5c6121e4f8d0a07f2d43
dhruv <dr26dhruv@gmail.com>
2023-10-30 18:54:38 +0530

    error resolve

--- 12MegaBlog_Appwrite/src/store/authSlice.js
+++ 12MegaBlog_Appwrite/src/store/authSlice.js
@@ -30,3 +30,29 @@ const authSlice = createSlice({
 export const {/* export the actions/methods */ login,logout  } = authSlice.actions;
 
 export default authSlice.reducer
+
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   status: false,
+//   userData: null,
+// };
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState,
+//   reducers: {
+//     login: (state, action) => {
+//       state.status = true;
+//       state.userData = action.payload.userData;
+//     },
+//     logout: (state) => {
+//       state.status = false;
+//       state.userData = null;
+//     },
+//   },
+// });
+
+// export const { login, logout } = authSlice.actions;
+
+// export default authSlice.reducer;
--- 12MegaBlog_Appwrite/src/store/store.js
+++ 12MegaBlog_Appwrite/src/store/store.js
@@ -9,3 +9,15 @@ const store = configureStore({
 });
 
 export default store;
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import authSlice from "./authSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authSlice,
+//     //TODO: add more slices here for posts
+//   },
+// });
+
+// export default store;
