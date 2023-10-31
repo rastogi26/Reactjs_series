1c37d0db5eb15cc5cb7633e7eab1bfe19123abb5
dhruv <dr26dhruv@gmail.com>
2023-10-31 16:17:03 +0530

    logout

--- 12MegaBlog_Appwrite/src/components/Header/Header.jsx
+++ 12MegaBlog_Appwrite/src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Link } from " react-router-dom";
+import { useNavigate } from " react-router-dom";
+import {useSelector  } from "react-redux";
+
+
 
 const Header = () => {
+  const authStatus = useSelector((state)=>state.auth.status)
+  const navigate = useNavigate()
   return (
     <div>
       Header
--- 12MegaBlog_Appwrite/src/components/container/Container.jsx
+++ 12MegaBlog_Appwrite/src/components/container/Container.jsx
@@ -1,1 +1,10 @@
 import React from 'react'
+
+//used for styling component
+
+function Container({children}) {
+  return <div className=' w-full  max-w-7xl mx-auto py-4'> {children} </div>;
+  
+}
+
+export default Container
