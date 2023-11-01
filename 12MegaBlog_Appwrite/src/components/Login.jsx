352122febec539e91476622a19d90a45f334d3e1
Dhruv Rastogi <72301136+rastogi26@users.noreply.github.com>
2023-11-01 10:28:13 +0000

    login

--- 12MegaBlog_Appwrite/src/components/Login.jsx
+++ 12MegaBlog_Appwrite/src/components/Login.jsx
@@ -37,8 +37,11 @@ const Login = () => {
     <div
       className='flex items-center justify-center w-full ' 
     >
-        <div className=' mx-auto w-full  max-w-lg  bg-gray-100 rounded'>
-
+        <div className={`mx-auto w-full  max-w-lg  bg-gray-100 rounded-xl p-10 border border-black/10 `}>
+          <div className=' mb-2 flex justify-center'>
+              
+          </div>
+              
         </div>
     </div>
   )
